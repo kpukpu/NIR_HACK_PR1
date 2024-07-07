3622d19102df3a6c4bd67392497646899a10b070
esalyc <yunchancode@naver.com>
2024-07-07 15:08:52 +0900

    nav 디자인

--- frontend/src/Navigator/Nav.js
+++ frontend/src/Navigator/Nav.js
@@ -1,20 +1,78 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import React from "react";
-import './Nav.css'
+import styled from "styled-components";
 
 const Nav = () => {
+    const StyledElement = styled.div`
+        background-color: #ffffff;
+        height: 4.3em;
+        position: relative;
+
+
+        & .NIR-HACK {
+            color: #000000;
+            font-weight: 400;
+            left: 20px;
+            letter-spacing: 1px;
+            line-height: normal;
+            position: absolute;
+            white-space: nowrap;
+        }
+
+        & .text-wrapper {
+            font-size: 2em;
+            font-weight: 700;
+            letter-spacing: 1px;
+        }
+
+        & .span {
+            font-size: 1.5em;
+            letter-spacing: 1px;
+        }
+
+        & .frame {
+            align-items: flex-start;
+            display: inline-flex;
+            gap: 21px;
+            left: 381px;
+            position: absolute;
+            top: 0;
+        }
+
+        & .linkTo {
+            color: #000000;
+            font-size: 1.3em;
+            font-weight: 400;
+            height: 70px;
+            letter-spacing: 1px;
+            line-height: normal;
+            margin-top: 1em;
+            position: relative;
+            text-align: center;
+            width: 216px;
+            text-decoration: none;
+        }
+    `;
+
+
     return (
-        <div>
-            <div className="navbar">
-                <Link className="navbarMenu" to={'/'}> 홈 화면 </Link>
-                <Link className="navbarMenu" to={'/TodoList'}> To-do 리스트 </Link>
-                <Link className="navbarMenu" to={'/Calculate'}> 계산기 </Link>
-                <Link className="navbarMenu" to={'/DischargeCalculate'}> 전역일 계산기 </Link>
-                <Link className="navbarMenu" to={'/DDayCalculatorPage'}> 디데이 계산기 </Link>
-                <Link className="navbarMenu" to={'/TodayWeather'}> 오늘의 날씨 </Link>
+        <StyledElement>
+            <p className="NIR-HACK">
+                <span className="text-wrapper">NIR </span>
+                <span className="span">HACK</span>
+            </p>
+
+            <div className="frame">
+                <Link className="linkTo" to={'/TodoList'}> To-do 리스트 </Link>
+                <Link className="linkTo" to={'/Calculate'}> 계산기 </Link>
+                <Link className="linkTo" to={'/DischargeCalculate'}> 전역일 계산기 </Link>
+                <Link className="linkTo" to={'/DDayCalculatorPage'}> 디데이 계산기 </Link>
+                <Link className="linkTo" to={'/TodayWeather'}> 오늘의 날씨 </Link>
             </div>
-        </div>
+        </StyledElement>
     );
+
+
 };
 
 export default Nav;
